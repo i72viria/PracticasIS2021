01ff91758a2fd3d06a7b5ed2df913f842e14290e
alvarorl231 <aroldanlucena@gmail.com>
2020-12-15 11:21:54 +0100

    cambios

--- ruta.cc
+++ ruta.cc
@@ -5,15 +5,10 @@ using namespace std;
 
 Ruta::Ruta(int codigo, string nombre, string estado, string dificultad, float longitud, float duracion)
 {
-    codigo=codigo_;
-    nombre=nombre_;
-    estado=estado_;
-    dificultad=dificultad_;
-    longitud=longitud_;
-    duracion=duracion_;
-}
-
-void Ruta::getRutas()
-{
-    string rutas[6];
+    setCodigo(codigo);
+    setNombre(nombre);
+    setEstado(estado);
+    setDificultad(dificultad);
+    setLongitud(longitud);
+    setDuracion(duracion);
 }
--- ruta.h
+++ ruta.h
@@ -8,8 +8,6 @@
 
 using namespace std;
 
-struct ;
-
 class Ruta
 {
     private:
@@ -25,19 +23,19 @@ class Ruta
 
         //MODIFICADORES
         inline bool setCodigo(int codigo){if(codigo=000000)return false; else codigo_=codigo; return true;}
-        inline void setNombre(string nombre){nombre=nombre_;}
-        inline void setEstado(string estado){estado=estado_;}
-        inline void setDificultad(stirng dificultad){dificultad=dificultad_;}
-        inline void setLongitud(float longitud){longitud=longitud_;}
-        inline void setDuracion(float duracion){duracion=duracion_;}
+        inline void setNombre(string nombre){nombre_=nombre;}
+        inline void setEstado(string estado){estado_=estado;}
+        inline void setDificultad(string dificultad){dificultad_=dificultad;}
+        inline void setLongitud(float longitud){longitud_=longitud;}
+        inline void setDuracion(float duracion){duracion_=duracion;}
 
         //OBSERVADORES       
         inline int getCodigo()const{return codigo_;}
-        inline void setNombre()const{return nombre_;}
-        inline void setEstado()const{return estado_;}
-        inline void setDificultad()const{return dificultad_;}
-        inline void setLongitud()const{return longitud_;}
-        inline void setDuracion()const{return duracion_;}
+        inline string getNombre()const{return nombre_;}
+        inline string getEstado()const{return estado_;}
+        inline string getDificultad()const{return dificultad_;}
+        inline float getLongitud()const{return longitud_;}
+        inline float getDuracion()const{return duracion_;}
 
 
         //OTRAS FUNCIONES
