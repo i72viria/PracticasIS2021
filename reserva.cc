ebe5ac63b831c0ff83e880455e1f8513a9161884
victoriapl01 <i92prlav@uco.es>
2020-12-15 20:07:36 +0100

    Continuacion reserva.cc

---
+++ reserva.cc
@@ -0,0 +1,8 @@
+#include "reserva.h"
+
+Reserva::Reserva(string fecha_reserva, string dni_reserva, string tipo_ruta){
+   setFechaReserva(fecha_reserva);
+   setDniReserva(dni_reserva);
+   setTipoRuta(tipo_ruta);
+}
+
