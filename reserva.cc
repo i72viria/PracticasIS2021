4703238947f31d425a5b9ed3e4ce6551829bcced
victoriapl01 <i92prlav@uco.es>
2020-12-16 11:06:30 +0100

    Modificacion y comentarios clase reserva

--- reserva.cc
+++ reserva.cc
@@ -1,5 +1,6 @@
 #include "reserva.h"
 
+//CONSTRUCTOR
 Reserva::Reserva(string fecha_reserva, string dni_reserva, string tipo_ruta){
    setFechaReserva(fecha_reserva);
    setDniReserva(dni_reserva);
--- reserva.h
+++ reserva.h
@@ -10,17 +10,23 @@ using namespace std;
 
 class Reserva{
     private:
-        date fecha_reserva_;
+        string fecha_reserva_;
         string dni_reserva_;  
         string tipo_ruta_;
 
     public:
-        inline void setFechaReserva(date fecha_reserva){fecha_reserva_=fechareserva;}
-        inline date getFechaReserva(){return fecha_reserva_;}
+        //CONSTRUCTOR
+        Reserva(string fecha_reserva="", string dni_reserva="", string tipo_ruta="");
 
+        //MODIFICADOR Y OBSERVADOR FECHA RESERVA
+        inline void setFechaReserva(string fecha_reserva){fecha_reserva_=fecha_reserva;}
+        inline string getFechaReserva(){return fecha_reserva_;}
+
+        //MODIFICADOR Y OBSERVADOR DNI RESERVA
         inline void setDniReserva(string dni_reserva){dni_reserva_=dni_reserva;}
         inline string getDniReserva(){return dni_reserva_}
 
+        //MODIFICADOR Y OBSERVADOR TIPO RUTA
         inline void setTipoRuta(string tipo_ruta){tipo_ruta_=tipo_ruta;}
         inline string getTipoRuta(){return tipo_ruta_;}
 };
