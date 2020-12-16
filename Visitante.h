a9736bc248983120ed8a543a8c6863daae761f95
i72viria <i72viria@uco.es>
2020-12-16 12:44:08 +0100

    corrección de errores de la clase Visitante

--- Visitante.h
+++ Visitante.h
@@ -28,25 +28,25 @@ using namespace std;
           TelefonoV_=TelefonoV;
           Discapacidad_=Discapacidad;
         }
-        inline void setNombreV(string NombreV){NombreV=NombreV_;}
+        inline void setNombreV(string NombreV){NombreV_=NombreV;}
         inline string getNombreV(){return NombreV_;}
 
-        inline void setApellido1V(string Apellido1V){Apellido1V=Apellido1V_;}
-        inline string getApellido1V(){return getApellido1V_;}
+        inline void setApellido1V(string Apellido1V){Apellido1V_=Apellido1V;}
+        inline string getApellido1V(){return Apellido1V_;}
 
-        inline void setApellido2V(string Apellido2V){Apellido2V=Apellido2V_;}
+        inline void setApellido2V(string Apellido2V){Apellido2V_=Apellido2V;}
         inline string getApellido2V(){return Apellido2V_;}
 
-        inline void setDniV(string DniV){DniV=DniV_;}
+        inline void setDniV(string DniV){DniV_=DniV;}
         inline string getDniV(){return DniV_;}
 
-        inline void setFecha_nacimiento(date Fecha_nacimiento){Fecha_nacimiento=Fecha_nacimiento_;}
-        inline date getFecha_nacimiento(){return Fechanacimiento_;}
+        inline void setFecha_nacimiento(date Fecha_nacimiento){Fecha_nacimiento_=Fecha_nacimiento;}
+        inline date getFecha_nacimiento(){return Fecha_nacimiento_;}
 
-        inline void setTelefonoV(int TelefonoV){TelefonoV=TelefonoV_;}
+        inline void setTelefonoV(int TelefonoV){TelefonoV_=TelefonoV;}
         inline int getTelefonoV(){return TelefonoV_;}
 
-        inline void setDiscapacidad(string Discapacidad){Discapacidad=Discapacidad_;}
+        inline void setDiscapacidad(string Discapacidad){Discapacidad_=Discapacidad;}
         inline string getDiscapacidad(){return Discapacidad_;}
 
 };
