c35286a2dda9e7a3f0ebec9bc5f4bd730d06977c
alvarorl231 <aroldanlucena@gmail.com>
2020-12-17 17:18:26 +0100

    Finalización de la ultima funcion en la clase rutas

--- ruta.cc
+++ ruta.cc
@@ -12,3 +12,34 @@ Ruta::Ruta(int codigo, string nombre, string estado, string dificultad, float lo
     setLongitud(longitud);
     setDuracion(duracion);
 }
+
+void Ruta::getRutas()
+{
+    fstream f;
+    f.open("rutas.txt",std::fstream::in);
+    if(f.is_open()){
+        char codigo[225],nombre[225],estado[225],dificultad[225],longitud[225],
+            duracion[225];
+        
+        while(f.getline(codigo,225,',')){
+            f.getline(nombre,225,',');
+            f.getline(estado,225,',');
+            f.getline(dificultad,225,',');
+            f.getline(longitud,225,',');
+            f.getline(duracion,225,'\n');
+
+            cout<<codigo<<" "<<nombre<<" "<<estado<<" "<<dificultad<<" "<<longitud<<" "<<duracion<<"\n";
+        }
+    }
+        else{
+            cout<<"Error. No se pudo abrir el fichero";
+        }
+    f.close();
+}
+
+int main()
+{
+    Ruta t;
+    t.getRutas();
+    return 0;
+}
--- ruta.h
+++ ruta.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 
 using namespace std;
 
