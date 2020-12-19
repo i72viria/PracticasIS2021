d5a3511267dc449566caac1d221a16156bbff1f5
alvarorl231 <aroldanlucena@gmail.com>
2020-12-19 11:28:33 +0100

    Cambio EXPECT por ASSERT en pruebas unitarias

--- googletest/administrativo_unittest.cc
+++ googletest/administrativo_unittest.cc
@@ -1,13 +1,4 @@
 // administrativo_unittest.cc: Álvaro Roldán Lucena
-// A sample program demonstrating using Google C++ testing framework.
-//
-// This sample shows how to write a more complex unit test for a class
-// that has multiple member functions.
-//
-// Usually, it's a good idea to have one test for each method in your
-// class.  You don't have to do that exactly, but it helps to keep
-// your tests organized.  You may also throw in additional tests as
-// needed.
 
 #include <fstream>
 #include <list>
@@ -22,99 +13,99 @@ using namespace std;
 TEST(Administrativo, ConstructorParametrosDefecto) {
   Administrativo j;
   
-  EXPECT_EQ("", j.getNombre());
-  EXPECT_EQ("", j.getApellido1());
-  EXPECT_EQ("", j.getApellido2());
-  EXPECT_EQ("", j.getDni());
-  EXPECT_EQ("", j.getDireccion());
-  EXPECT_EQ(0, j.getTelefono());
+  ASSERT_EQ("", j.getNombre());
+  ASSERT_EQ("", j.getApellido1());
+  ASSERT_EQ("", j.getApellido2());
+  ASSERT_EQ("", j.getDni());
+  ASSERT_EQ("", j.getDireccion());
+  ASSERT_EQ(0, j.getTelefono());
 }
 
 TEST(Administrativo, ConstructorParametros) {
   Administrativo j("Alvaro","Roldan","Lucena","123456789X","Cordoba","i92rolua@uco.es",123456789);
   
-  EXPECT_EQ("Alvaro", j.getNombre());
-  EXPECT_EQ("Roldan", j.getApellido1());
-  EXPECT_EQ("Lucena", j.getApellido2());
-  EXPECT_EQ("123456789X", j.getDni());
-  EXPECT_EQ("Cordoba", j.getDireccion());
-  EXPECT_EQ(123456789, j.getTelefono());
+  ASSERT_EQ("Alvaro", j.getNombre());
+  ASSERT_EQ("Roldan", j.getApellido1());
+  ASSERT_EQ("Lucena", j.getApellido2());
+  ASSERT_EQ("123456789X", j.getDni());
+  ASSERT_EQ("Cordoba", j.getDireccion());
+  ASSERT_EQ(123456789, j.getTelefono());
 }
 
 TEST(Administrativo, setNombreygetNombre) {
   Administrativo j("Pepe");
 
-  EXPECT_EQ("Pepe", j.getNombre());
+  ASSERT_EQ("Pepe", j.getNombre());
   j.setNombre("Paco");
-  EXPECT_EQ("Paco", j.getNombre());
+  ASSERT_EQ("Paco", j.getNombre());
 }
 
 TEST(Administrativo, setApellido1ygetApellido1) {
   Administrativo j("Pepe","Rodriguez");
 
-  EXPECT_EQ("Rodriguez", j.getApellido1());
+  ASSERT_EQ("Rodriguez", j.getApellido1());
   j.setApellido1("Lozano");
-  EXPECT_EQ("Lozano", j.getApellido1());
+  ASSERT_EQ("Lozano", j.getApellido1());
 }
 
 TEST(Administrativo, setApellido2ygetApellido2) {
   Administrativo j("Pepe","Rodriguez","Roldan");
 
-  EXPECT_EQ("Roldan", j.getApellido2());
+  ASSERT_EQ("Roldan", j.getApellido2());
   j.setApellido2("Laguna");
-  EXPECT_EQ("Laguna", j.getApellido2());
+  ASSERT_EQ("Laguna", j.getApellido2());
 }
 
 TEST(Administrativo, setDniygetDni) {
   Administrativo j("Pepe","Rodriguez","Roldan","123X");
 
-  EXPECT_EQ("123X", j.getDni());
+  ASSERT_EQ("123X", j.getDni());
   j.setDni("876A");
-  EXPECT_EQ("876A", j.getDni());
+  ASSERT_EQ("876A", j.getDni());
 }
 
 TEST(Administrativo, setDireccionygetDireccion) {
   Administrativo j("Pepe","Rodriguez","Roldan","123X","Madrid");
 
-  EXPECT_EQ("Madrid", j.getDireccion());
+  ASSERT_EQ("Madrid", j.getDireccion());
   j.setDireccion("Bilbao");
-  EXPECT_EQ("Bilbao", j.getDireccion());
+  ASSERT_EQ("Bilbao", j.getDireccion());
 }
 
 TEST(Administrativo, setEmailygetEmail) {
   Administrativo j("Pepe","Rodriguez","Roldan","123X","Madrid","pepe@uco.es");
 
-  EXPECT_EQ("pepe@uco.es", j.getEmail());
+  ASSERT_EQ("pepe@uco.es", j.getEmail());
   j.setEmail("rodri@uco.es");
-  EXPECT_EQ("rodri@uco.es", j.getEmail());
+  ASSERT_EQ("rodri@uco.es", j.getEmail());
 }
 
 
 TEST(Administrativo, setTelefonoygetTelefono) {
   Administrativo j("Pepe","Rodriguez","Roldan","123X","Madrid","pepe@uco.es",123);
 
-  EXPECT_EQ(123, j.getTelefono());
+  ASSERT_EQ(123, j.getTelefono());
   j.setTelefono(999);
-  EXPECT_EQ(999, j.getTelefono());
+  ASSERT_EQ(999, j.getTelefono());
 }
 
 TEST(Administrativo, getUsuario) {
   Administrativo j("Pepe","Rodriguez","Roldan","123X","Madrid","pepe@uco.es",123);
 
-  EXPECT_EQ("PepeRodriguezRoldan", j.getUsuario());
+  ASSERT_EQ("PepeRodriguezRoldan", j.getUsuario());
   j.setNombre("Paco");
   j.setApellido1("Lozano");
   j.setApellido2("Laguna");
   j.setUsuario();
-  EXPECT_EQ("PacoLozanoLaguna", j.getUsuario());
+  ASSERT_EQ("PacoLozanoLaguna", j.getUsuario());
 }
 TEST(Administrativo, getContrasena) {
   Administrativo j("Pepe","Rodriguez","Roldan","123X","Madrid","pepe@uco.es",123);
 
-  EXPECT_EQ("123X", j.getContrasena());
+  ASSERT_EQ("123X", j.getContrasena());
   j.setDni("968A");
   j.setContrasena();
-  EXPECT_EQ("968A", j.getContrasena());
+  ASSERT_EQ("968A", j.getContrasena());
 }
 
 TEST(Administrativo, crearReserva) {
@@ -124,11 +115,11 @@ TEST(Administrativo, crearReserva) {
   Reserva j("12/12/2020","123456789X","Bicicleta");
   Reserva w("15/08/2020","987654321A","A pie");
 
-  EXPECT_TRUE(m.creaReserva(j));
-  EXPECT_TRUE(m.creaReserva(w));
+  ASSERT_TRUE(m.creaReserva(j));
+  ASSERT_TRUE(m.creaReserva(w));
 
-  EXPECT_FALSE(m.creaReserva(j));
-  EXPECT_FALSE(m.creaReserva(w));
+  ASSERT_FALSE(m.creaReserva(j));
+  ASSERT_FALSE(m.creaReserva(w));
 }
 
 
@@ -137,18 +128,18 @@ TEST(Administrativo, deleteDatos) {
   Administrativo m;
   string nombre="";
 
-  EXPECT_EQ(-1,m.deleteDatos(nombre));
+  ASSERT_EQ(-1,m.deleteDatos(nombre));
   
   Parque j("Pedroches","Cordoba",540.54,"21/03/1989",123456789,"09:00 a 20:00 L-V");
 
   m.crearParque(j);
   nombre=j.getNombre();
 
-  EXPECT_EQ(1,m.deleteDatos(nombre));
+  ASSERT_EQ(1,m.deleteDatos(nombre));
 
   m.crearParque(j);
   nombre="Pepe";
-  EXPECT_EQ(-2,m.deleteDatos(nombre));
+  ASSERT_EQ(-2,m.deleteDatos(nombre));
 }
 
 TEST(Administrativo, crearParque) {
@@ -158,11 +149,11 @@ TEST(Administrativo, crearParque) {
   Parque i("Pedroches","Cordoba",540.54,"21/03/1989",123456789,"09:00 a 20:00 L-V");
   Parque j("Caminito del Rey","Malaga",320.19,"01/11/19759",987654321,"09:00 a 20:00 L-J y 12:00 a 14:00 S-D");
 
-  EXPECT_TRUE(m.crearParque(i));
-  EXPECT_TRUE(m.crearParque(j));
+  ASSERT_TRUE(m.crearParque(i));
+  ASSERT_TRUE(m.crearParque(j));
 
-  EXPECT_FALSE(m.crearParque(i));
-  EXPECT_FALSE(m.crearParque(j));
+  ASSERT_FALSE(m.crearParque(i));
+  ASSERT_FALSE(m.crearParque(j));
 }
 
 TEST(Administrativo, identificacion) {
@@ -170,7 +161,7 @@ TEST(Administrativo, identificacion) {
 
   cout<<"\n"<<"El usuario es PepeRodriguezRoldan y la contrasena 123X"<<endl;
 
-  EXPECT_TRUE(j.identificacion());
+  ASSERT_TRUE(j.identificacion());
   cout<<"\n"<<"Ahora introducir mal."<<endl;
-  EXPECT_FALSE(j.identificacion());
+  ASSERT_FALSE(j.identificacion());
 }
